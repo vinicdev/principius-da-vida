3e23078245925cb134d6a72232503334e99f3900
Vinicius Cardoso <vinic8670@gmail.com>
2023-12-12 21:31:03 -0300

    adicionando imagens e arquivo para gerenciamento

---
+++ utils/images.js
@@ -0,0 +1,11 @@
+export const images = {
+    logo: {
+        src: 'logo-principius-da-vida.svg',
+        alt: 'Ensino de qualidade em Curitiba - PR'
+    },
+
+    bgHeader: {
+        src: 'ensino-de-qualidade.svg',
+        alt: 'Escola em Colombo - PR'
+    }
+}
