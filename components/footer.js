2d0c502f6c05c024b7e8d7691f07fabc07b90070
Vinicius Cardoso <vinic8670@gmail.com>
2024-01-03 13:33:26 -0300

    criando componente footer

---
+++ components/footer.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const Container = styled.section`
+    padding: 40px;
+`;
+
+export default function Footer() {
+    return (
+        <Container>
+            Footer
+        </Container>
+    )
+}
